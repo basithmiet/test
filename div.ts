7064d8117586ccc20f1b216b17c7d3c223a115f1
Abdul Basith <39832836+basithmiet@users.noreply.github.com>
2025-02-06 01:24:50 +0530

    Update div.ts

--- div.ts
+++ div.ts
@@ -1,28 +1,12 @@
-
-addDatePicker() {
-  setTimeout(() => {
-    var self = this;    
-    (function () {
-      var elements = document.getElementsByClassName('date-field');
-      console.log(elements);
-      if (elements && elements.length > 0) {
-        for (var i = 0; i < elements.length; i++) {
-          var id = elements[i].id; // Get the ID correctly
-          $('#' + id).datepicker({
-            dateFormat: "mm/dd/yy",
-            appendTo: "body"
-          });
-          $('#' + id).datepicker('setDate', ''); // Corrected method
-          $("body").on("change", "#" + id, function () {
-            console.log(id);
-            $('#' + id).trigger("click");
-          });
-          $('#' + id).on("change", function (dp) {
-            var value = dp.currentTarget.value;
-            console.log(id, value);
-          });
-        }
-      }
-    })();
-  });
-}
+$('#' + id).datepicker({
+  dateFormat: "mm/dd/yy",
+  appendTo: "body",
+  beforeShow: function(input, inst) {
+    setTimeout(function() {
+      inst.dpDiv.css({
+        top: $(input).offset().top + $(input).outerHeight(),
+        left: $(input).offset().left
+      });
+    }, 0);
+  }
+});
